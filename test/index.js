0ab9dce2dde08320f69d2e8404a3676999ae502d
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-07 20:06:01 -0400

    test(tranformFile): add a test for transformFile
    Adds a simple test that checks the output of transformFile

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import test from 'tape'
 import { sum, head } from './prelude'
 import { add, Circle, sum as sampleSum } from './sample'
+import { transformFile } from '../index'
 
 test('sum should add up the list', t => {
   t.plan(2)
@@ -28,4 +29,41 @@ test('Circle should have properties on the prototype', t => {
 test('sampleSum should be the same as sum', t => {
   t.plan(1)
   t.equal(sampleSum([5, 5, 7]), 17)
+})
+
+test('transformFile should convert files to ES5/commonjs', t => {
+  t.plan(1)
+
+  const { code } = transformFile('test/sample.js')
+
+  function standardizeString (str) {
+    return str.replace(/\s/g, '')
+  }
+
+  /* eslint-disable */
+  const expected = `'use strict';                                                                                                                                             
+                                                                                                                                                          
+Object.defineProperty(exports, "__esModule", {                                                                                                            
+  value: true                                                                                                                                             
+});                                                                                                                                                       
+exports.sum = exports.Circle = exports.add = undefined;                                                                                                   
+                                                                                                                                                          
+var _prelude = require('./prelude');                                                                                                                      
+                                                                                                                                                          
+var add = function (a, b) { return a + b; };                                                                                                              
+                                                                                                                                                          
+var Circle = function Circle(radius) {                                                                                                                    
+  this.radius = this;                                                                                                                                     
+};                                                                                                                                                        
+                                                                                                                                                          
+Circle.prototype.area = function area () {                                                                                                                
+  return Math.PI * Math.pow(this.radius, 2);                                                                                                              
+};                                                                                                                                                        
+                                                                                                                                                          
+exports.add = add;                                                                                                                                        
+exports.Circle = Circle;                                                                                                                                  
+exports.sum = _prelude.sum;`
+  /* eslint-enable */
+
+  t.equal(standardizeString(code), standardizeString(expected))
 })
